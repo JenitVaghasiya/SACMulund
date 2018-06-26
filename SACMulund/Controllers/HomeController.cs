1d1764290e1f7b122f81f89dfa69d013cb472aa9
JenitVaghaisya <jenits.212@gmail.com>
2018-06-27 00:21:20 +0530

    email

--- SACMulund/Controllers/HomeController.cs
+++ SACMulund/Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿
+using MimeKit;
+using SACMulund.Models;
+using System.Web.Configuration;
 using System.Web.Mvc;
+using MailKit.Net.Smtp;
+using System;
+using MailKit.Security;
+using System.Net;
+using MimeKit.Text;
 
 namespace SACMulund.Controllers
 {
@@ -25,6 +30,60 @@ namespace SACMulund.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public async System.Threading.Tasks.Task<JsonResult> sendmail(string name, string email, string number)
+        {
+            if (!string.IsNullOrEmpty(name) || !string.IsNullOrEmpty(email) || !string.IsNullOrEmpty(number))
+            {
+                EmailConfig ec = new EmailConfig
+                {
+                    FromAddress = WebConfigurationManager.AppSettings["FromAddress"],
+                    FromName = WebConfigurationManager.AppSettings["FromName"],
+                    MailServerAddress = WebConfigurationManager.AppSettings["MailServerAddress"],
+                    MailServerPort = WebConfigurationManager.AppSettings["MailServerPort"],
+                    UserId = WebConfigurationManager.AppSettings["UserId"],
+                    UserPassword = WebConfigurationManager.AppSettings["UserPassword"]
+                };
+
+
+                try
+                {
+                    var emailMessage = new MimeMessage();
+
+                    emailMessage.From.Add(new MailboxAddress(ec.FromName, ec.FromAddress));
+                    emailMessage.To.Add(new MailboxAddress("", email));
+                    emailMessage.Subject = "Contact request";
+                    emailMessage.Body = new TextPart(TextFormat.Html) { Text = "<html><body><table><tr><td colspan='2'>Contact request detail</td></tr><tr><tr><td>Name:</td><td>" + name + "</td></tr><tr><td>Email:</td><td>" + email  + "</td></tr><tr><td>Phone no:</td><td>" + number + "</td></tr></table></body></html>" };
+
+                    using (var client = new SmtpClient())
+                    {
+                        var credentials = new NetworkCredential
+                        {
+                            UserName = ec.UserId,
+                            Password = ec.UserPassword
+                        };
+
+                        //client.LocalDomain = "xyz.dk";
+                        await client.ConnectAsync(ec.MailServerAddress, Convert.ToInt32(ec.MailServerPort), SecureSocketOptions.Auto).ConfigureAwait(false);
+                        client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                        await client.AuthenticateAsync(credentials);
+
+                        await client.SendAsync(emailMessage);
+                        await client.DisconnectAsync(true).ConfigureAwait(false);
+                        //You need to add return here
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+            return Json(true);
         }
     }
 }
---
+++ SACMulund/Models/EmailConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SACMulund.Models
+{
+    public class EmailConfig
+    {
+        public String FromName { get; set; }
+        public String FromAddress { get; set; }
+
+
+        public String MailServerAddress { get; set; }
+        public String MailServerPort { get; set; }
+
+        public String UserId { get; set; }
+        public String UserPassword { get; set; }
+    }
+}
